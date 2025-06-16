867bcbc2f39d5407b620674ec8a90fbfe8d53e4f
Artem Pchelintsev <pchelintsev.art@gmail.com>
2025-06-16 22:49:47 +0300

    Решение в рамках ФЗ

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,65 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final Map<Long, User> users = new HashMap<>();
+    private long nextId = 1;
+    private final static Logger log = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping
+    public Collection<User> getAllUsers() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User createUser(@Valid @RequestBody User user) {
+        validateUser(user);
+        user.setId(getNextId());
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User updateUser(@Valid @RequestBody User user) {
+        if (user.getId() == null || !users.containsKey(user.getId())) {
+            throw new ValidationException("Пользователь с таким id не существует.");
+        }
+        validateUser(user);
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    private void validateUser(User user) {
+        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            throw new ValidationException("Электронная почта не может быть пустой и должна содержать символ @.");
+        }
+        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            throw new ValidationException("Логин не может быть пустым и содержать пробелы.");
+        }
+        if (user.getBirthday() != null && user.getBirthday().isAfter(java.time.LocalDate.now())) {
+            throw new ValidationException("Дата рождения не может быть в будущем.");
+        }
+    }
+
+    private long getNextId() {
+        return nextId++;
+    }
+}
